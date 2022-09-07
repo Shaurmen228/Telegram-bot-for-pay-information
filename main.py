27d33b8dd739ec8441e5a2f227c250a8d0fd5327
artem <artem-dedov2012@yandex.ru>
2022-09-07 19:56:55 +0300

    Округление ЗП до целого числа

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import telebot
 
 bot = telebot.TeleBot('5751300078:AAExYVTnPW7WsPSzn-iGxAbpIl5aufZ6ztk')
@@ -6,7 +7,7 @@ bot = telebot.TeleBot('5751300078:AAExYVTnPW7WsPSzn-iGxAbpIl5aufZ6ztk')
 @bot.message_handler(commands=['start'])
 def start(message):
     pid = message.from_user.id
-    if str(pid) == '1000105342':
+    if str(pid) == '1000105342' or str(pid) == '372210652':
         bot.send_message(message.chat.id, 'Привет зайка. Введи сумму оборота твоих продаж: ')
     else:
         bot.send_message(message.chat.id, 'Я чувствую что ты не зая, сука')
@@ -18,7 +19,8 @@ def get_user_text(message):
     if message.text.isdigit():
         sum_oborot = float(message.text)
         itog = sum_oborot * 0.00484 + 25000
-        bot.send_message(message.chat.id, 'Твоя примераная зарплата равна: ' + str(itog) + " вечно деревянных")
+        bot.send_message(message.chat.id,
+                         'Твоя примераная зарплата равна: ' + str(math.ceil(itog)) + " вечно деревянных")
     else:
         bot.send_message(message.chat.id, 'Зай,ты накосячила с вводом цифры, не вводи символы или этот бот умрет')
 
